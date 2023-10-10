b34317a42f6db0e2372a8ba8cf23a0712b662c14
Jayanti2919 <jayanti2919@gmail.com>
2023-10-10 20:37:08 +0530

    added form to upload

--- client/src/components/Encrypt.jsx
+++ client/src/components/Encrypt.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Encrypt = () => {
-  return (
-    <div>Encrypt</div>
-  )
-}
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [key, setKey] = useState('');
 
-export default Encrypt
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleKeyChange = (event) => {
+    setKey(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+    formData.append('key', key);
+    const url = 'http://127.0.0.1:5000/encrypt';
+    
+    try {
+        const response = await axios.post(url, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        const data=await response
+        console.log('Image and key uploaded successfully:', data.data);
+    } catch (error) {
+        console.error('Error uploading image and key:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Image and Key Upload</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <input type="text" placeholder="Enter key" onChange={handleKeyChange} />
+        <button type="submit">Upload</button>
+      </form>
+    </div>
+  );
+};
+
+export default Encrypt;
--- server/app.py
+++ server/app.py
@@ -1,21 +1,65 @@
 from flask import Flask, request, jsonify, send_from_directory
 from blowfish import Blowfish
+from PIL import Image
+import numpy as np
+import os
+from flask_cors import CORS
+# import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 # blowfish = None
+CORS(app)
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
 
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    image = request.files['image']
+
+    if image.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    img = Image.open(image)
+
+    # Convert the image to a NumPy array
+    img_array = np.array(img)
+    print(img_array)
+
+    return jsonify({'no error': 'ok'}), 200
 
 @app.route('/encrypt', methods=['POST'])
 def encrypt_data():
-    data = request.json.get('data')  # Assuming you receive data as JSON
-    # global blowfish 
-    blowfish = Blowfish(request.json.get('key'))
-    encrypted_data = blowfish.blowFish_encrypt(data)
-    return jsonify({'encrypted_data': encrypted_data})
+    if 'image' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    image = request.files['image']
+
+    if image.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    img = Image.open(image)
+    img_array = np.array(img)
+
+    # print(img_array)
+    blowfish = Blowfish(request.form.get('key'))
+
+    
+    encrypted_array = np.zeros_like(img_array, dtype=np.uint64)
+    for i in range(img_array.shape[0]):
+        for j in range(img_array.shape[1]):
+            for k in range(img_array.shape[2]):
+                encrypted_array[i][j][k]=(blowfish.blowFish_encrypt(int(img_array[i][j][k])))
+    print(encrypted_array)
+    # encrypted_list = encrypted_array.tolist()
+
+    return jsonify({'data': "ok"}), 200
+
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt_data():
